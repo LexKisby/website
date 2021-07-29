42cb3be38a48c48924573e1e64bb1b92ffb91743
LexKisby <alexander.r.kisby@durham.ac.uk>
2021-07-29 21:10:58 +0100

    Canvas element resizes automagically

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,14 @@
 const canvas = document.querySelector("canvas");
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    resizeCanvas();
+}, false);
+
+window.addEventListener('resize', function(event){
+    resizeCanvas();
+  });
