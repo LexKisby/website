02d6cade7c902e2ee0b36ae5f172de4d0e064401
LexKisby <alexander.r.kisby@durham.ac.uk>
2021-07-30 18:31:26 +0100

    bloom filters enabled

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -14,7 +14,7 @@ container.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x000002);
 
-const camera = new THREE.PerspectiveCamera(45,	window.innerWidth / window.innerHeight, 1, 500);
+const camera = new THREE.PerspectiveCamera(45,	window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.z = 300;
 camera.position.y = 0;
 scene.add(camera);
@@ -31,7 +31,7 @@ const bloomParams = {
     bloomThreshold: 0,
     exposure: 0.9,
     bloomStrength: 3,
-    bloomRadius: 0.52
+    bloomRadius: 0.3
 };
 
 const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
@@ -45,17 +45,49 @@ composer.addPass(bloomPass);
 
 //GEOMETRY
 ////////////////////////////////
-const modelGeometry = new THREE.IcosahedronBufferGeometry(70);
-const wireFrame = new THREE.WireframeGeometry(modelGeometry);
-const line = new THREE.LineSegments(wireFrame);
-line.material.depthTest = false;
-line.material.opacity = 0.4;
+var num_tris = 150;
+var geometry = [];
 
-line.material.transparent = true;
-line.position.x = -20;
-scene.add(line);
+const geo = new THREE.BoxGeometry(10,10,10); 
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
+const cube = new THREE.Mesh( geo, material ); 
+scene.add( cube ); 
 
-const model = line;
+
+
+function initGeometry() {
+    const g = new THREE.TetrahedronGeometry(14);
+    const w = new THREE.WireframeGeometry(g);
+    const m = new THREE.LineBasicMaterial({color: 0xbb3434, linewidth: 20});
+    for (let i = 0; i < num_tris; i++) {
+        var object = new THREE.LineSegments(w,m);
+
+        object.scale.x = Math.random() * 2 + 1;
+	    object.scale.y = Math.random() * 2 + 1;
+	    object.scale.z = Math.random() * 2 + 1;
+
+
+        object.position.x = 400*(Math.random() * 2 -1);
+        object.position.y = 400*(Math.random() * 2 -1);
+        object.position.z = 1300*(Math.random() -1);
+
+        object.rotation.x = Math.random() * 2 * Math.PI;
+	    object.rotation.y = Math.random() * 2 * Math.PI;
+	    object.rotation.z = Math.random() * 2 * Math.PI;
+
+        
+        object.material.depthTest = false;
+        object.material.opacity = 0.4;
+
+        object.material.transparent = true;
+
+        scene.add(object);
+        geometry.push(object);
+
+
+    }
+}
+initGeometry();
 
 
 
@@ -73,10 +105,20 @@ let counter = 0;
 function animate() {
     requestAnimationFrame(animate);
     composer.render();
-    model.rotation.x += 0.02;
-	model.rotation.y += -0.02;
-	model.rotation.z += 0.03;
-	model.position.x += Math.sin(counter);
+    for (var i = 0; i<num_tris; i++) {
+        geometry[i].rotation.x += 0.002;
+        geometry[i].rotation.y += 0.001;
+        geometry[i].rotation.z += 0.003;
+
+    
+        //geometry[i].position.x += 0.3;
+        geometry[i].position.z += 2;
+        if (geometry[i].position.z > 300) {
+            geometry[i].position.z = -1000;
+        }
+
+
+    }
 	counter += 0.05;
     
 }
