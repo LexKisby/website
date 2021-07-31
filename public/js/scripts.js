52bd50c77615baaf262a90f2cad54dc2f538637c
LexKisby <alexander.r.kisby@durham.ac.uk>
2021-07-31 21:55:32 +0100

    barba very promising

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,15 @@
 barba.init();
 
-
+var links = document.querySelectorAll( 'a[href]' );
+var cbk = function ( e ) {
+    if ( e.currentTarget.href === window.location.href ) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+};
+for ( var i = 0; i < links.length; i++ ) {
+    links[ i ].addEventListener( 'click', cbk );
+}
 
 
 const body = document.querySelector( 'body' );
